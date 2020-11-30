35478ae366367a3a4aebd2eb5c1bf58cb158d391
kmskumaar <kmskumaar@gmail.com>
2020-11-30 20:53:05 +0100

    - [NEW FUNCTIONALITY] copy point cloud and mesh to device/gpu memory

--- pcl/cuda/cuda_common.cpp
+++ pcl/cuda/cuda_common.cpp
@@ -1,7 +1,7 @@
 #include "cuda_common.h"
 
 template<>
-bool pclcuda::copyToHostMem(pcl::PointCloud<float>& inCloud, pclcuda::PointCloudHost<float4>& outCloudHost) {
+bool pclcuda::copyPtCldToHostMem(pcl::PointCloud<float>& inCloud, pclcuda::PointCloudHost<float4>& outCloudHost) {
 	size_t noOfPts = inCloud.size();
 	outCloudHost.resize(noOfPts);
 
@@ -12,12 +12,60 @@ bool pclcuda::copyToHostMem(pcl::PointCloud<float>& inCloud, pclcuda::PointCloud
 }
 
 template<>
-bool pclcuda::copyToHostMem(pcl::PointCloud<double>& inCloud, pclcuda::PointCloudHost<double4>& outCloudHost) {
+bool pclcuda::copyPtCldToHostMem(pcl::PointCloud<double>& inCloud, pclcuda::PointCloudHost<double4>& outCloudHost) {
 	size_t noOfPts = inCloud.size();
 	outCloudHost.resize(noOfPts);
 
 	for (size_t i = 0; i < noOfPts; i++)
 		outCloudHost[i] = make_double4(inCloud[i].x, inCloud[i].y, inCloud[i].z, 1.0);
+
+	return true;
+}
+
+template <>
+bool pclcuda::copyMeshToHostMem(pcl::PolygonMesh<float>& inMesh, pclcuda::PointCloudHost<float4>& vertices_1,
+	pclcuda::PointCloudHost<float4>& vertices_2, pclcuda::PointCloudHost<float4>& vertices_3, pclcuda::PlaneParamHost<float4>& planeParam) {
+
+	size_t noOfPolygons = inMesh.polygon.size();
+	vertices_1.resize(noOfPolygons);
+	vertices_2.resize(noOfPolygons);
+	vertices_3.resize(noOfPolygons);
+	planeParam.resize(noOfPolygons);
+	for (size_t idx = 0; idx < noOfPolygons; idx++)
+	{
+		planeParam[idx] = make_float4(inMesh.planes[idx].normal.i, inMesh.planes[idx].normal.j, inMesh.planes[idx].normal.k,
+			inMesh.planes[idx].D);
+		vertices_1[idx] = make_float4(inMesh.cloud[inMesh.polygon[idx].indices[0]].x, inMesh.cloud[inMesh.polygon[idx].indices[0]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[0]].z, 1.0);
+		vertices_2[idx] = make_float4(inMesh.cloud[inMesh.polygon[idx].indices[1]].x, inMesh.cloud[inMesh.polygon[idx].indices[1]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[1]].z, 1.0);
+		vertices_3[idx] = make_float4(inMesh.cloud[inMesh.polygon[idx].indices[2]].x, inMesh.cloud[inMesh.polygon[idx].indices[2]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[2]].z, 1.0);
+	}
+
+	return true;
+}
+
+template <>
+bool pclcuda::copyMeshToHostMem(pcl::PolygonMesh<double>& inMesh, pclcuda::PointCloudHost<double4>& vertices_1,
+	pclcuda::PointCloudHost<double4>& vertices_2, pclcuda::PointCloudHost<double4>& vertices_3, pclcuda::PlaneParamHost<double4>& planeParam) {
+
+	size_t noOfPolygons = inMesh.polygon.size();
+	vertices_1.resize(noOfPolygons);
+	vertices_2.resize(noOfPolygons);
+	vertices_3.resize(noOfPolygons);
+	planeParam.resize(noOfPolygons);
+	for (size_t idx = 0; idx < noOfPolygons; idx++)
+	{
+		planeParam[idx] = make_double4(inMesh.planes[idx].normal.i, inMesh.planes[idx].normal.j, inMesh.planes[idx].normal.k,
+			inMesh.planes[idx].D);
+		vertices_1[idx] = make_double4(inMesh.cloud[inMesh.polygon[idx].indices[0]].x, inMesh.cloud[inMesh.polygon[idx].indices[0]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[0]].z, 1.0);
+		vertices_2[idx] = make_double4(inMesh.cloud[inMesh.polygon[idx].indices[1]].x, inMesh.cloud[inMesh.polygon[idx].indices[1]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[1]].z, 1.0);
+		vertices_3[idx] = make_double4(inMesh.cloud[inMesh.polygon[idx].indices[2]].x, inMesh.cloud[inMesh.polygon[idx].indices[2]].y,
+			inMesh.cloud[inMesh.polygon[idx].indices[2]].z, 1.0);
+	}
 
 	return true;
 }
--- pcl/cuda/cuda_common.h
+++ pcl/cuda/cuda_common.h
@@ -1,12 +1,22 @@
 #pragma once
 #include "../pcl/pcl_base.h"
 #include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 #include <vector_functions.hpp>
 
 
 namespace pclcuda {
 	template<typename T>
 	using  PointCloudHost = thrust::host_vector<T>;
+
+	template<typename T>
+	using  PlaneParamHost = thrust::host_vector<T>;
+
+	template<typename T>
+	using  PointCloudDevice = thrust::device_vector<T>;
+
+	template<typename T>
+	using  PlaneParamDevice = thrust::device_vector<T>;
 
 }
 
@@ -18,5 +28,15 @@ namespace pclcuda {
 	[return] boolean - true is process succeeded/ else false
 	*/
 	template <typename T1, typename T2>
-	bool copyToHostMem(pcl::PointCloud<T1>& inCloud, pclcuda::PointCloudHost<T2>& outCloudHost);
+	bool copyPtCldToHostMem(pcl::PointCloud<T1>& inCloud, pclcuda::PointCloudHost<T2>& outCloudHost);
+
+	/*
+	Copies the content of the mesh into the host memory thrust vector.
+	[in] inCloud - Pointer to the cloud
+	[out] outCloudHost - Pointer to the cloud in the thrust host vector
+	[return] boolean - true is process succeeded/ else false
+	*/
+	template <typename T1, typename T2>
+	bool copyMeshToHostMem(pcl::PolygonMesh<T1>& inMesh, pclcuda::PointCloudHost<T2>& vertices_1, pclcuda::PointCloudHost<T2>& vertices_2, pclcuda::PointCloudHost<T2>& vertices_3,
+		pclcuda::PlaneParamHost<T2>& planeParam);
 }
